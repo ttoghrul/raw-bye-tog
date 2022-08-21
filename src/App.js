a189e77249d0fa7e97c79df7f520b4d4bc66d366
Toghrul Taghiyev <ttoghrul@gmail.com>
2022-08-21 12:46:13 +0100

    Added page with grey background and list into it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
 import logo from './logo.svg';
 import './App.css';
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import * as React from 'react';
+import {Paper} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*<header className="App-header">*/}
+                {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+                {/*  <p>*/}
+                {/*    Edit <code>src/App.js</code> and save to reload.*/}
+                {/*  </p>*/}
+                {/*  <a*/}
+                {/*    className="App-link"*/}
+                {/*    href="https://reactjs.org"*/}
+                {/*    target="_blank"*/}
+                {/*    rel="noopener noreferrer"*/}
+                {/*  >*/}
+                {/*    Learn React*/}
+                {/*  </a>*/}
+                {/*</header>*/}
+
+                <Paper elevation={13} background-color='grey'>
+                    <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Brunch this weekend?"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{display: 'inline'}}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            Ali Connors
+                                        </Typography>
+                                        {" — I'll be in your neighborhood doing errands this…"}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li"/>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg"/>
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Summer BBQ"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{display: 'inline'}}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            to Scott, Alex, Jennifer
+                                        </Typography>
+                                        {" — Wish I could come, but I'm out of town this…"}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li"/>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg"/>
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Oui Oui"
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{display: 'inline'}}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            Sandra Adams
+                                        </Typography>
+                                        {' — Do you have Paris recommendations? Have you ever…'}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                    </List>
+                </Paper>
+            </header>
+        </div>
+    );
 }
 
 export default App;
